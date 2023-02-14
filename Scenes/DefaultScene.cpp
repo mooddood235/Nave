959f4247ae4a941571d1eca16c0270202b82fce3
unknown <mahmoud.zeidan@mail.utoronto.ca>
2023-02-14 16:07:59 -0500

    Added ARB_bindless_texture extension

--- Scenes/DefaultScene.cpp
+++ Scenes/DefaultScene.cpp
@@ -5,7 +5,7 @@ DefaultScene::DefaultScene() {
     mathSpheres = {
         MathSphere(Material(glm::vec3(1.0f, 0.0, 0.0f), 0.0f, 0.0f, glm::vec3(0.0f))),
         MathSphere(glm::vec3(-2.0f, 0.0, 0.0f), 1.0f, Material(glm::vec3(1.0f), 0.0f, 1.0f, glm::vec3(0.0f))),
-        MathSphere(glm::vec3(-1.0f, 0.0f, -2.0f), 1.0f, Material(glm::vec3(0.0f, 0.0f, 1.0f), 0.05f, 0.0f, glm::vec3(10.0f))),
+        MathSphere(glm::vec3(-1.0f, 0.0f, -2.0f), 1.0f, Material(glm::vec3(0.0f, 0.0f, 1.0f), 0.05f, 0.0f, glm::vec3(0.0f))),
         MathSphere(glm::vec3(0.0f, -51.0f, 0.0f), 50.0f, Material(glm::vec3(1.0), 1.0f, 0.0f, glm::vec3(0.0f)))
     };
     
@@ -13,10 +13,12 @@ DefaultScene::DefaultScene() {
     bunny.Scale(glm::vec3(15.0f));
 
     Model cube = Model("Models/Cube/Cube.fbx");
-    //cube.Rotate(45, glm::vec3(1, 0, 0));
-    //cube.Translate(glm::vec3(0, 4, 0));
 
-    models = { bunny, cube};
+
+    bunny.Translate(glm::vec3(2, 0, 0));
+    cube.Translate(glm::vec3(2, 0, 0));
+
+    models = { bunny, cube };
 
     GenerateSSBOs();
 }
